839a8e7670dedc61a51a529af3fd2e0e003688e6
Allan Aguilar <alan-joc@hotmail.com>
2024-10-05 12:35:29 -0600

    Created css file and GiphyFinderApp FC

---
+++ src/GiphyFinderApp.tsx
@@ -0,0 +1,14 @@
+import { FC, useState } from 'react';
+
+export const GiphyFinderApp: FC = () => {
+
+    const [] = useState(['Dragon Ball']);
+
+  return (
+    <>
+        <h2>Giphy Finder App</h2>
+    </>
+  );
+}
+
+
