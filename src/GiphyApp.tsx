c80c5fcc2dcda43fd4c2823779a1b8c24d124033
Allan Aguilar <alan-joc@hotmail.com>
2024-10-05 21:47:44 -0600

    Rename Repo App

--- src/GiphyApp.tsx
+++ src/GiphyApp.tsx
@@ -2,17 +2,18 @@ import { FC, useState } from 'react';
 import { AddCategory } from './components/AddCategory';
 import giphy from '../public/giphy.svg'
 
-export const GiphyFinderApp: FC = () => {
+export const GiphyApp: FC = () => {
 
-  const [categories, setCategories] = useState<string[]>(['DBZ']);
+  const [ categories, setCategories ] = useState<string[]>( ['DBZ'] );
 
   return (
     <div className="flex flex-col p-[60px]">
 
       <div className="flex items-center mb-4">
         <img src={giphy} alt="Giphy Logo" className="h-8 mr-2" />
-        <h1 className="text-4xl text-white">Giphy Finder App</h1>
+        <h1 className="text-4xl text-white font-bold">GIPHY Finder App</h1>
       </div>
+
       <AddCategory setCategories={setCategories} />
 
       <hr className="my-4 border-gray-600" />
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { GiphyFinderApp } from './GiphyFinderApp';
+import { GiphyApp } from './GiphyApp';
 
 import './index.css'; // Global CSS Styles
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <GiphyFinderApp />
+        <GiphyApp />
     </React.StrictMode>
 )
